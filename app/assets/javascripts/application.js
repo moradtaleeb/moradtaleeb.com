4e524eafd156a0b70e8fdcfd74b7a0f423d95fe5
moradtaleeb <morad.taleeb@gmail.com>
2013-12-03 11:47:11 +0200

    layout and design

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,58 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function(){
+
+  $(".left-nav").click(function(){
+    $(".intro").css("position","absolute");
+    $("body").css("overflow","hidden");
+    $( ".main-side" ).animate({
+      left: "3000px"
+    }, 1000 );
+  });
+
+  $(".back").click(function(){
+    $("body").css("overflow-y","auto");
+    $(".main-side").show();
+    $( ".main-side" ).animate({
+      left: "0px"
+    }, 500, function(){
+      $(".intro").css("position","fixed");
+    });
+
+    /* $( ".main-side" ).animate({
+      left: "0px"
+    }, {
+      duration: 500,
+      specialEasing: {
+        width: "easeOutBounce"
+      },
+      complete: function() {
+        $(".intro").css("position","fixed");
+      }
+    }); */
+
+  });
+
+  $(window).scroll(function(){
+    offset = $(window).scrollTop();
+
+    if( offset > 10 ) {
+      $(".left-nav").css("left", "-100px").fadeOut().find("p").hide();
+      $(".right-nav").css("right", "-100px").fadeOut();
+    }
+    else {
+      $(".left-nav").fadeIn(50).css("left", "-40px");
+      $(".right-nav").fadeIn(50).css("right", "-40px");
+    }
+
+    if( offset > 220 ) {
+      $(".intro .container").css({"top":"-160px", "opacity": 0 });
+    }
+    else {
+      $(".intro .container").css({"top":"0px", "opacity": 1 });
+    }
+  });
+
+});
