37bfce3e8fda093c8ca57d99fee019c66f35bb79
moradtaleeb <morad.taleeb@gmail.com>
2013-12-12 00:13:17 +0200

    changed default url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Discover::Application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
-  # config.action_mailer.default_url_options = { :host => 'www.moradtaleeb.com' }
+  config.action_mailer.default_url_options = { :host => 'http://fathomless-lake-8876.herokuapp.com' }
 
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
