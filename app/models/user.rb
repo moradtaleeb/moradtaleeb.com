a884b70f713fdd6a230d45dae19baf1e9beb2081
moradtaleeb <morad.taleeb@gmail.com>
2013-12-19 01:15:59 +0200

    no sign up + fb parse

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :rememberable, :trackable, :validatable, :registerable
+  devise :database_authenticatable, :rememberable, :trackable, :validatable
+  #, :registerable
 end
